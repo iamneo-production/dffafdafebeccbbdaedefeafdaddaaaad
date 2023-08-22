8524606e43fb71968ba0d2f2a1fe8b7cffabcdbc
Iamneo <techadmin@iamneo.ai>
2023-08-22 18:13:49 +0000

    Backup Commit

--- DBFirstApproach/Program.cs
+++ DBFirstApproach/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DBFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ""
+ 
+
+            // Rest of your code
+        }
+    }
+}
+User ID=sa;password=examlyMssql@123;server=localhost;Database=DBFirstApproach;trusted_connection=false;Persist Security Info=False;Encrypt=False;
